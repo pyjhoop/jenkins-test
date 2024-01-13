1bcdc6f2102f3759b0dbd6e30a04ac0c4c2bb4a9
pyjhoop <pyjhoop@naver.com>
2024-01-13 17:27:20 +0900

    test

--- src/main/java/com/jenkins/test/TestApplication.java
+++ src/main/java/com/jenkins/test/TestApplication.java
@@ -2,12 +2,18 @@ package com.jenkins.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TestApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:real-application.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+		new SpringApplicationBuilder(TestApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);;
 	}
-	//test
 }
